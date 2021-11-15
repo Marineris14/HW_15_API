f83a8fecd6dd0d82b3d5886030a6c3e7b4f1a0ca
DESKTOP-G6IS2T2\Toshiba <marineriscr@gmail.com>
2021-11-15 23:13:52 +0200

    Add SignInModel

---
+++ HW_15_API_Auto/HW_15_API_Auto/SignInRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Tests_API
+{
+    public class SignInRequestModel
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        public SignInRequestModel(string email, string name, string password)
+        {
+            this.Email = email;
+            this.Name = name;
+            this.Password = password;
+        }
+    }
+}
