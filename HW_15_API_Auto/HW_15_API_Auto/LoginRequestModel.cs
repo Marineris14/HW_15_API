87baff80eb135f2c01f87bd8fc707e95ec4f97ac
DESKTOP-G6IS2T2\Toshiba <marineriscr@gmail.com>
2021-11-15 23:18:53 +0200

    Add LoginRequestModel

---
+++ HW_15_API_Auto/HW_15_API_Auto/LoginRequestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tests_API
+{
+    public class LoginRequestModel
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        public LoginRequestModel(string email, string password)
+        {
+            this.Email = email;
+            this.Password = password;
+        }
+    }
+}
