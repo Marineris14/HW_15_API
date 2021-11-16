6e10b4e88e4d36f243ad9cb6d118ace20062112b
DESKTOP-G6IS2T2\Toshiba <marineriscr@gmail.com>
2021-11-16 09:09:32 +0200

    Add New createuser model

---
+++ HW_15_API_Auto/HW_15_API_Auto/CreateUserWithTasksModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tests_API
+{
+    public class CreateUserWithTasksModel
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("tasks")]
+        public List<Task> Tasks { get; set; }
+
+        [JsonProperty("companies")]
+        public List<int> Companies { get; set; }
+
+        [JsonProperty("hobby")]
+        public string Hobby { get; set; }
+
+        [JsonProperty("adres")]
+        public string Adres { get; set; }
+
+        [JsonProperty("name1")]
+        public string Name1 { get; set; }
+
+        [JsonProperty("surname1")]
+        public string Surname1 { get; set; }
+
+        [JsonProperty("fathername1")]
+        public string Fathername1 { get; set; }
+
+        [JsonProperty("cat")]
+        public string Cat { get; set; }
+
+        [JsonProperty("dog")]
+        public string Dog { get; set; }
+
+        [JsonProperty("parrot")]
+        public string Parrot { get; set; }
+
+        [JsonProperty("cavy")]
+        public string Cavy { get; set; }
+
+        [JsonProperty("hamster")]
+        public string Hamster { get; set; }
+
+        [JsonProperty("squirrel")]
+        public string Squirrel { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("inn")]
+        public string Inn { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("birthday")]
+        public string Birthday { get; set; }
+
+
+        [JsonProperty("date_start")]
+        public string Date_start { get; set; }
+
+    }
+}
